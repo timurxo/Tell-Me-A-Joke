58a454328181c34b2301c4ce53b2b0ab819c328d
timurxo <45484568+timurxo@users.noreply.github.com>
2020-10-03 18:52:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+var my_key = config.myKey;
+var jokes_url = config2.jokesUrl;
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// pass joke to speech api
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, '%20');
+  
+  VoiceRSS.speech({
+    key: my_key,
+    src: jokeString,
+    hl: 'en-us',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
+
+// Get jokes from Joke API
+async function getJokes() {
+  let joke = '';
+    const apiUrl = jokes_url;
+  try {
+    // return json object
+    const response = await fetch(apiUrl);
+    // extract json from response
+    const data = await response.json();
+
+    // Assign One or Two Part Joke
+    if (data.setup) {   // for 2 part jokes
+        // create pause for 2 part jokes
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+
+    // text to speech
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    
+  }
+}
+
+// Event listeners
+button.addEventListener('click', getJokes);
+// enable button only after audio is finished
+audioElement.addEventListener('ended', toggleButton);   
